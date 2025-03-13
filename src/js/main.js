7ad86a1146db636f2af9e380e4bf0de445b3fb8c
Bakhriev <legendofpain5112@gmail.com>
2025-03-13 13:15:10 +0500

    catalog-menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,46 @@
 import { initModal } from "./modal.js";
+import { focusTrap } from "./focusTrap.js";
+
+const initCatalogMenu = triggerSelector => {
+	const catalogMenu = document.querySelector(".catalog-menu");
+	const catalogTrigger = document.querySelector(triggerSelector);
+
+	if (!catalogMenu | !catalogTrigger) return;
+
+	const buttons = catalogMenu.querySelectorAll(".catalog-menu__btn");
+	const panels = catalogMenu.querySelectorAll(".catalog-menu__panel");
+
+	const toggleVisible = index => {
+		buttons.forEach(btn => btn.classList.remove("active"));
+		buttons[index].classList.add("active");
+
+		panels.forEach(panel => panel.classList.remove("active"));
+		panels[index].classList.add("active");
+	};
+
+	buttons.forEach(btn => {
+		btn.addEventListener("click", () => {
+			toggleVisible(btn.dataset.tab);
+			focusTrap(catalogMenu);
+		});
+	});
+
+	catalogTrigger.addEventListener("click", () => {
+		if (catalogMenu.classList.contains("active")) {
+			catalogMenu.classList.remove("active");
+			catalogTrigger.focus();
+		} else {
+			catalogMenu.classList.add("active");
+			focusTrap(catalogMenu);
+		}
+	});
+
+	catalogMenu.addEventListener("keydown", e => {
+		if (e.key === "Escape") {
+			catalogMenu.classList.remove("active");
+			catalogTrigger.focus();
+		}
+	});
+};
+
+initCatalogMenu("[data-trigger='catalog-menu']");
